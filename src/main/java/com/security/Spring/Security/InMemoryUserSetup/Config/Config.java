fa51e382e50ccf91c2a854c9ff771bed98dec53b
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-02-26 00:06:44 +0900

    added second user, using User class

--- src/main/java/com/security/Spring/Security/InMemoryUserSetup/Config/Config.java
+++ src/main/java/com/security/Spring/Security/InMemoryUserSetup/Config/Config.java
@@ -5,19 +5,26 @@ import com.security.Spring.Security.InMemoryUserSetup.Entity.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import java.util.List;
+
 @Configuration
 @ComponentScan(basePackages = "com.security.Spring.Security.InMemoryUserSetup")
 public class Config {
+
     @Bean
     public UserDetailsService userDetails(){
         var user=new UserService(new BlogUser("alenaxon","1234","admin"));
-        return new InMemoryUserDetailsManager(user);
+        var user2 = User.withUsername("khan")
+                .authorities("reader")
+                .password("111")
+                .build();
+        return new InMemoryUserDetailsManager(List.of(user,user2));
     }
 
     @Bean
